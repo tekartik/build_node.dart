4724dc973a0afe66ff48a2323ea91cdeccabaa31
alex <alex@tekartik.com>
2022-02-08 13:22:59 +0100

    fix: node build

---
+++ packages/example/simple_example/test/simple_test.dart
@@ -0,0 +1,7 @@
+import 'package:test/test.dart';
+
+void main() {
+  test('simple', () {
+    expect(true, isTrue);
+  });
+}
--- packages/example/simple_example_support/test/build_and_run_test.dart
+++ packages/example/simple_example_support/test/build_and_run_test.dart
@@ -1,16 +1,17 @@
 import 'package:dev_test/build_support.dart';
-import 'package:test/test.dart';
-
-import 'package:simple_example_support/run_node.dart' as run;
 import 'package:simple_example_support/build_node.dart' as build;
+import 'package:simple_example_support/run_node.dart' as run;
+import 'package:simple_example_support/test_node.dart';
+import 'package:test/test.dart';
 
 void main() {
   group('node', () {
     test('build_and_run', () async {
-      if (isNodeSupportedSync) {
-        await build.main();
-        await run.main();
-      }
+      await build.main();
+      await run.main();
     }, timeout: Timeout(Duration(minutes: 5)));
-  });
+    test('dart test -p node', () async {
+      await runNodeTest();
+    }, timeout: Timeout(Duration(minutes: 1)));
+  }, skip: !isNodeSupportedSync ? 'Node not supported' : null);
 }
