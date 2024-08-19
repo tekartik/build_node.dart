a235a8b453a7417c0f28f1293cad675070d3288c
Alexandre Roux <alex@tekartik.com>
2024-08-19 10:21:05 +0200

    fix ci

--- packages/example/simple_example/node/main.dart
+++ packages/example/simple_example/node/main.dart
@@ -1,7 +1,6 @@
 // ignore: depend_on_referenced_packages
 import 'package:tekartik_common_utils/common_utils_import.dart';
 import 'package:tekartik_common_utils/env_utils.dart';
-import 'package:tekartik_common_utils/src/debug.dart';
 
 void main() {
   print('Hello world');
